da7434e2372b2b528363a74d6ccaf927a16bf0d5
viertor <lukasz.mosinski@gmail.com>
2021-05-27 15:43:32 +0200

    Adding external api client

--- BlazorDemo/Program.cs
+++ BlazorDemo/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using JsonPlaceholder.Adapter;
 
 namespace BlazorDemo
 {
@@ -18,6 +19,8 @@ namespace BlazorDemo
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new Client());
+
 
             await builder.Build().RunAsync();
         }
---
+++ JsonPlaceholder.Adapter/Client.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace JsonPlaceholder.Adapter
+{
+    public class Client
+    {
+        public static async System.Threading.Tasks.Task<List<Models.User>> GetUsersAsync()
+        {
+     
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //GET Method  
+                HttpResponseMessage response = await client.GetAsync("users");
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<List<Models.User>>(await jsonString);
+
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
